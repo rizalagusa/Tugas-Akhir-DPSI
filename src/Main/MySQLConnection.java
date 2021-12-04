d33b1947f62b3814f09070b2275b960913739430
Rizal <rizal_agus_26rpl@student.smktelkom-mlg.sch.id>
2021-12-04 14:36:32 +0700

    first commit

---
+++ src/Main/MySQLConnection.java
@@ -0,0 +1,24 @@
+package Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class MySQLConnection {
+    Connection conn = null;
+    
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db_sysbook","root","");
+            return conn;
+        }catch(ClassNotFoundException | SQLException ex){
+            JOptionPane.showMessageDialog(null, ex);
+            JOptionPane.showMessageDialog(null, "Not Connected to Databases/Turn on Your Xampp");
+            return null;
+        }
+    
+    }
+}
